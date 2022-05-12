c1089654a27ed34f561f5caff7720a6958f26a4f
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-05-12 14:12:04 -0300

    Add App component

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import reactDom from "react-dom";
+
+function App() {
+    return (
+        <>
+            <p>fala meu chegas</p>
+        </>
+    )
+}
+
+reactDom.render(<App />, document.querySelector(".root"))
